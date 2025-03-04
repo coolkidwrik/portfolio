d4c3eb8969f8a02d97a53a2b6cfe1815736f20f4
Wrik <coolkidwrik@gmail.com>
2025-03-04 12:45:54 -0800

    changed directory structure and added three.js utils

---
+++ src/utils/setup.ts
@@ -0,0 +1,45 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+function setup(canvas: HTMLCanvasElement) {
+    // Create a scene
+    const w = window.innerWidth;
+    const h = window.innerHeight;
+    const scene = new THREE.Scene();
+
+    // Create a camera
+    const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+    camera.position.z = 5;
+
+    // Create a renderer
+    const renderer = new THREE.WebGLRenderer({
+        canvas,
+        antialias: true
+    });
+    renderer.shadowMap.enabled = true;
+    renderer.setSize(w, h);
+
+    renderer.toneMapping = THREE.ReinhardToneMapping;
+    renderer.toneMappingExposure = 2.0;
+
+    // Setup orbit controls for the camera.
+    new OrbitControls(camera, renderer.domElement);
+
+    // Handle window resize
+    function handleWindowResize() {
+        let w = window.innerWidth;
+        let h = window.innerHeight;
+        camera.aspect = w / h;
+        camera.updateProjectionMatrix();
+        renderer.setSize(w, h);
+    }
+    window.addEventListener('resize', handleWindowResize, false);
+
+    return {
+        renderer,
+        scene,
+        camera
+    };
+}
+
+export { setup };
