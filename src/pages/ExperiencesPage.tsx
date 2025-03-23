5ef681861db0302dd55d1936d9f94800af31bbe5
Wrik <coolkidwrik@gmail.com>
2025-03-22 19:55:25 -0700

    modified page layouts

--- src/pages/ExperiencesPage.tsx
+++ src/pages/ExperiencesPage.tsx
@@ -1,14 +1,20 @@
 import { Link } from "react-router-dom";
 import PageTransition from "../utils/pageTransition";
+import Footer from "../utils/footer";
 
 const ExperiencesPage = () => {
   return (
     <PageTransition>
-      <div className="Experiences content">
-        <h1>Experiences</h1>
-        <p>Under Construction</p>
-        <Link to="/">Go Back to Home</Link>
+      <div className="Experiences page-content">
+        <div className="absolute top-[20%] left-[8%] text-white flex flex-col items-center">
+          <h1 className="text-[5rem]">Experiences</h1>
+        </div>
+        <div>
+          <p>Under Construction</p>
+          <Link to="/">Go Back to Home</Link>
+        </div>
       </div>
+      <Footer />
     </PageTransition>
   );
 };
--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
+import PageTransition from "../utils/pageTransition";
+import Footer from "../utils/footer";
 
 const ExperiencesPage = () => {
   return (
-    <div className="Projects content">
-        <h1>Projects</h1>
-        <p>Under Construction</p>
-        <Link to="/">Go Back to Home</Link>
-    </div>
+    <PageTransition>
+      <div className="Projects page-content">
+          <h1>Projects</h1>
+          <p>Under Construction</p>
+          <Link to="/">Go Back to Home</Link>
+      </div>
+      <Footer />
+    </PageTransition>
   );
 };
 
